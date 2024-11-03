1adbce7f3190419dcc58af5d935928a6bc4961d7
Oğuzhan Çevik <oguzhan.cevik@st.uskudar.edu.tr>
2024-11-03 17:37:48 +0300

    DecoratorPattern

---
+++ DesignPatterns.DecoratorPattern/Program.cs
@@ -0,0 +1,62 @@
+﻿
+
+
+
+
+ICoffe coffe1 = new BlackCoffe();
+Console.WriteLine($"{coffe1.Description}: {coffe1.Price()} TL");
+
+coffe1 = new MilkDecorator(coffe1);
+Console.WriteLine($"{coffe1.Description}: {coffe1.Price()} TL");
+
+
+
+
+public interface ICoffe
+{
+  string Description { get; }
+  double Price();
+}
+
+public class BlackCoffe : ICoffe
+{
+  public string Description => "Black Coffe";
+
+  public double Price()
+  {
+    return 100.0;
+  }
+}
+
+public abstract class CoffeDecorator : ICoffe
+{
+
+  public ICoffe _coffe { get; set; }
+
+  public CoffeDecorator(ICoffe coffe)
+  {
+    _coffe = coffe;
+  }
+  public virtual string Description => _coffe.Description;
+
+  public virtual double Price()
+  {
+    return _coffe.Price();
+  }
+}
+
+public class MilkDecorator : CoffeDecorator
+{
+  public MilkDecorator(ICoffe coffe) : base(coffe)
+  {
+  }
+
+  public override string Description => _coffe.Description+" Milk";
+
+  public override double Price()
+  {
+    return _coffe.Price() + 52;
+  }
+
+
+}
