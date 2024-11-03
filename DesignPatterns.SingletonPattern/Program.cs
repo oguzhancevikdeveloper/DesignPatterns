ecc916a4625ae3c28fd1acc22d36dfa0e54806e5
Oğuzhan Çevik <oguzhan.cevik@st.uskudar.edu.tr>
2024-11-03 19:10:11 +0300

    singleton pattern

---
+++ DesignPatterns.SingletonPattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using DesignPatterns.SingletonPattern;
+
+Console.WriteLine(DateTime.Now.ToLongTimeString());
+var countries = await CountryProvider.Instance.GetCountries();
+
+
+//foreach (var country in countries)
+//{
+//    Console.WriteLine(country.Name);
+//}
+
+// Another service
+
+var countries1 = await CountryProvider.Instance.GetCountries();
+
+Console.WriteLine(CountryProvider.Instance.CountryCount);
+
+Console.WriteLine(DateTime.Now.ToLongTimeString());
